2531cce2fe7b8abedf8205dabfb6c3cbf4e2e755
Frank Rodriguez <frank.aaron.rodriguez@gmail.com>
2022-05-13 17:31:23 -0500

    Added list of attach products for the authenticated user.
    Fixed being able to attach a product several times to a user.

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -9,11 +9,15 @@ class ProfileController extends Controller
     public function attachProduct(Request $request) 
     {
         $input = $request->validate(['product_id' => 'required|exists:products,id']);
-        $request->user()->products()->attach($input['product_id']);
+        $request->user()->products()->syncWithoutDetaching($input['product_id']);
     }
 
     public function detachProduct(Request $request) {
         $input = $request->validate(['product_id' => 'required|exists:products,id']);
         $request->user()->products()->detach($input['product_id']);
+    }
+
+    public function products(Request $request) {
+        return response()->json(['products' => $request->user()->products()->paginate()]);
     }
 }
